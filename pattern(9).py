0c84bfdf1f55d9931edf25e26549aa4d2627c84a
dhanish <98959425+Justdaniy@users.noreply.github.com>
2023-12-05 23:14:01 -0800

    Add files via upload

---
+++ pattern(9).py
@@ -0,0 +1,8 @@
+for i in range(0, 5):
+       for j in range(0, i+1):
+          print("*",end=" ")
+       print("\r")
+for n in range(5,0):
+       for m in range(n-1, 0):
+          print("*",end=" ")
+       print("\r")
